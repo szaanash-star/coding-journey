4ab86412f99cc870ef7ed2321b397975abe19bca
szaanash-star <szaanash@gmail.com>
2026-05-14 19:24:36 +0200

    Add Lesson 1: Hello World - First Python Program

---
+++ python-basics/01-hello-world/hello_world.py
@@ -0,0 +1,17 @@
+# Python Basics - Lesson 1: Hello World
+
+# This is your first Python program!
+# Lines that start with # are called "comments"
+# They explain what the code does but don't run as code
+
+# The print() function displays text on the screen
+print("Hello, World!")
+print("Welcome to my coding journey!")
+print("I'm learning Python on Linux Bazzite!")
+
+print()  # This prints an empty line
+
+# Let's print some decorative text
+print("=" * 50)
+print("Python is awesome!")
+print("=" * 50)
